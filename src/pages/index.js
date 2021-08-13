e65b36014135c5fe0534c14625545de0165238b9
petu <adamicska>
2021-08-13 19:38:51 -0300

    menu layout fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,28 @@
 import * as React from "react"
-// import { Link } from "gatsby"
+import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+
+import { Fragment } from "react"
+import { Popover, Transition } from "@headlessui/react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { PaperAirplaneIcon, MenuIcon, XIcon } from "@heroicons/react/outline"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faGithubAlt,
+  faStackOverflow,
+} from "@fortawesome/free-brands-svg-icons"
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons"
 
 import About from "../components/about"
 import Cta from "../components/cta"
 import Projects from "../components/projects"
-import { Link } from "gatsby"
+
+const navigation = [
+  { name: "About", href: "#about", current: false },
+  { name: "Projects", href: "#projects", current: false },
+]
 
 export default function IndexPage() {
   return (
@@ -26,6 +40,120 @@ export default function IndexPage() {
             >
               <polygon points="50,0 100,0 50,100 0,100" />
             </svg>
+            <Popover>
+              <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+                <nav
+                  className="relative flex items-center justify-between sm:h-10 lg:justify-start"
+                  aria-label="Global"
+                >
+                  <div className="flex items-center justify-between flex-grow flex-shrink-0 lg:flex-grow-0">
+                    <div className="flex items-center w-full md:w-auto">
+                      <PaperAirplaneIcon
+                        className="h-8 w-8 text-indigo-600"
+                        aria-hidden="true"
+                      />
+                      <strong className="text-gray-900 ">damicska</strong>
+                    </div>
+                    <div className="-mr-2 flex items-center md:hidden">
+                      <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                        <span className="sr-only">Open main menu</span>
+                        <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                      </Popover.Button>
+                    </div>
+                  </div>
+                  <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
+                    {navigation.map(item => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className="font-medium text-gray-500 hover:text-gray-900"
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                    <a
+                      href="https://github.com/adamicska"
+                      className="p-1 rounded-full text-gray-500 hover:text-gray-900 focus:outline-none"
+                    >
+                      <FontAwesomeIcon icon={faGithubAlt} size="lg" />
+                    </a>
+                    <a
+                      href="https://stackoverflow.com/users/11724751/adamicska"
+                      className="p-1 rounded-full text-gray-500 hover:text-gray-900 focus:outline-none"
+                    >
+                      <FontAwesomeIcon icon={faStackOverflow} size="lg" />
+                    </a>
+                    <a
+                      href="https://t.me/adamicska"
+                      className="p-1 rounded-full text-gray-500 hover:text-gray-900 focus:outline-none"
+                    >
+                      <FontAwesomeIcon icon={faPaperPlane} size="lg" />
+                    </a>
+                  </div>
+                </nav>
+              </div>
+
+              <Transition
+                as={Fragment}
+                enter="duration-150 ease-out"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="duration-100 ease-in"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Popover.Panel
+                  focus
+                  className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+                >
+                  <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+                    <div className="px-5 pt-4 flex items-center justify-between">
+                      <div>
+                        <PaperAirplaneIcon
+                          className="h-8 w-8 text-indigo-600"
+                          aria-hidden="true"
+                        />
+                      </div>
+                      <div className="-mr-2">
+                        <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                          <span className="sr-only">Close main menu</span>
+                          <XIcon className="h-6 w-6" aria-hidden="true" />
+                        </Popover.Button>
+                      </div>
+                    </div>
+                    <div className="px-2 pt-2 pb-3 space-y-1">
+                      {navigation.map(item => (
+                        <a
+                          key={item.name}
+                          href={item.href}
+                          className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                        >
+                          {item.name}
+                        </a>
+                      ))}
+                    </div>
+                    <a
+                      href="https://github.com/adamicska"
+                      className="block px-3 py-2 ml-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                    >
+                      <FontAwesomeIcon icon={faGithubAlt} size="lg" />
+                    </a>
+                    <a
+                      href="https://stackoverflow.com/users/11724751/adamicska"
+                      className="block px-3 py-2 ml-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                    >
+                      <FontAwesomeIcon icon={faStackOverflow} size="lg" />
+                    </a>
+                    <a
+                      href="https://t.me/adamicska"
+                      className="block px-3 py-2 ml-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                    >
+                      <FontAwesomeIcon icon={faPaperPlane} size="lg" />
+                    </a>
+                  </div>
+                </Popover.Panel>
+              </Transition>
+            </Popover>
             <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
               <div className="sm:text-center lg:text-left">
                 <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
@@ -69,8 +197,8 @@ export default function IndexPage() {
         <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
           <StaticImage
             className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-            src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
-            alt=""
+            src="../images/header_native_app_coding.jpg"
+            alt="native app developer"
           />
         </div>
       </div>
